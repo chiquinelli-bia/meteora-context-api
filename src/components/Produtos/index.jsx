4504ec152a7b093d5fe89ce4c28e51d6082739e5
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-03-19 08:32:48 -0300

    refactor(produtos): centraliza e refatora função de adicionar produto

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import { CarrinhoContext } from "@/context/contextCarrinho";
 
 const Produtos = ({}) => {
+  const [carrinho, setCarrinho] = useContext(CarrinhoContext);
+
+  function adicionarProduto(novoProduto) {
+    const temProduto = carrinho.some(
+      (produto) => produto.id === novoProduto.id,
+    );
+    if (!temProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto,
+      ]);
+    } else {
+      setCarrinho((carrinhoAnterior) =>
+        carrinhoAnterior.map((itemDoCarrinho) => {
+          if (itemDoCarrinho.id === novoProduto.id)
+            itemDoCarrinho.quantidade += 1;
+          return itemDoCarrinho;
+        }),
+      );
+    }
+  }
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
