f09b154194516faf207db39937b7159e38eba897
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-03-25 12:05:42 -0300

    feat: refatora listaProdutosCarrinho ee carrinho além de proteger handle Clicks e renderizações de possiveis erros.

--- src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
@@ -1,4 +1,3 @@
-import React, { useContext } from "react";
 import Quantidade from "@/components/Quantidade";
 import Botao from "@/components/Botao";
 import ValorFormatado from "@/components/ValorFormatado";
--- src/components/ListaProdutosCarrinho/index.jsx
+++ src/components/ListaProdutosCarrinho/index.jsx
@@ -1,39 +1,38 @@
-import React from "react";
 import ItemCarrinhoSuspenso from "@/components/CarrinhoSuspenso/ItemCarrinhoSuspenso";
 import ItemCarrinho from "@/components/ItemCarrinho";
 import { useLocation } from "react-router-dom";
+import { useCarrinho } from "@/hooks/useCarrinho";
 
-const ListaProdutosCarrinho = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-}) => {
+const ListaProdutosCarrinho = () => {
+  const { carrinho, adicionarProduto, removerProduto, removerProdutoCarrinho } =
+    useCarrinho();
   const location = useLocation();
   return (
     <ul className="list-unstyled">
       {carrinho.length === 0 ? (
         <p className="text-center my-5">Não há produtos no carrinho</p>
       ) : (
-        carrinho.map((itemCarrinho) => {
-          return location.pathname === "/carrinho" ? (
-            <ItemCarrinho
-              key={itemCarrinho.id}
-              itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
-            />
-          ) : (
-            <ItemCarrinhoSuspenso
-              key={itemCarrinho.id}
-              itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
-            />
-          );
-        })
+        carrinho
+          .filter((item) => item)
+          .map((itemCarrinho) => {
+            return location.pathname === "/carrinho" ? (
+              <ItemCarrinho
+                key={itemCarrinho.id}
+                itemCarrinho={itemCarrinho}
+                adicionarProduto={adicionarProduto}
+                removerProduto={removerProduto}
+                removerProdutoCarrinho={removerProdutoCarrinho}
+              />
+            ) : (
+              <ItemCarrinhoSuspenso
+                key={itemCarrinho.id}
+                itemCarrinho={itemCarrinho}
+                adicionarProduto={adicionarProduto}
+                removerProduto={removerProduto}
+                removerProdutoCarrinho={removerProdutoCarrinho}
+              />
+            );
+          })
       )}
     </ul>
   );
